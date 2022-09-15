d74cb8ad62de079ac278dadf0b71af593f9213eb
148413 <148413@hs-stemlt-06.district.bcs>
2022-09-15 09:01:39 -0500

    Merge origin/master

--- src/main/java/Exercise4_1.java
+++ src/main/java/Exercise4_1.java
@@ -2,10 +2,21 @@
 Seth Kelly
 9/15/22
 Exercise 4.1
-do things and stuff
+Do stuff and things
  */
-public class Exercise4_1 {
-    public static void main(String[] args){
-        
+import java.util.Scanner;
+public class Exercise4_1
+{
+    public static void main(String []args)
+    {
+        Scanner input = new Scanner(System.in);
+       
+        System.out.println("Enter the length from the center to a vertex:");
+        double length = input.nextDouble();
+       
+        double s = 2 * length * Math.sin(Math.PI / 5);
+        double area = ( (5 * s * s) / (4 * Math.tan(Math.PI / 5)) );
+        area = Math.round(area * 100) / 100.0;
+        System.out.println("The area of the pentagon is " + area);
     }
 }
